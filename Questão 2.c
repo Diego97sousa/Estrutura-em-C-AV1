db10715d2eb505057b008820353341039a0d4b49
Diego97sousa <116231789+Diego97sousa@users.noreply.github.com>
2023-04-26 20:17:46 -0300

    Update Questão 2.c

--- Questão 2.c
+++ Questão 2.c
@@ -2,16 +2,17 @@
 
 int main() {
     
-    int num;
+    int number_563;
     
+    printf("Questão 2\n\n");
     printf("Digite um número inteiro positivo: ");
-    scanf("%d", &num);
+    scanf("%d", &number_563);
 
-    if (num < 1) {
-        printf("O numero deve ser maior ou igual a 1.\n");
+    if (number_563< 1) {
+        printf("O numero maior ou igual a 1.\n");
     } else {
-        printf("Numeros impares menores ou iguais a %d e maiores ou iguais a 1:\n", num);
-        for (int i = 1; i <= num; i += 2) {
+        printf("Números impares menores ou iguais a %d e maiores ou iguais a 1:\n", number_563);
+        for (int i = 1; i <= number_563; i += 2) {
             printf("%d\n", i);
         }
     }
