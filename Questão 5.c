989e9c4ac2df82412f768c26215e2f4861299dcc
Diego97sousa <116231789+Diego97sousa@users.noreply.github.com>
2023-04-26 20:58:00 -0300

    Update Questão 5.c

--- Questão 5.c
+++ Questão 5.c
@@ -1,57 +1,57 @@
 #include <stdio.h>
 
-int maiorIdade(int a, int b, int c);
-int menorIdade(int a, int b, int c);
+int maiorIdade563(int a563, int b563, int c563);
+int menorIdade563(int a563, int b563, int c563);
 
 int main() {
-    int idade1, idade2, idade3;
-    int  maior, menor;
+    int idade1_563, idade2_563, idade3_563;
+    int  maior563, menor563;
 
+    printf("Questão 5\n\n");
     printf("Digite a idade da primeira pessoa: ");
-    scanf("%d", &idade1);
+    scanf("%d", &idade1_563);
 
     printf("Digite a idade da segunda pessoa: ");
-    scanf("%d", &idade2);
+    scanf("%d", &idade2_563);
 
     printf("Digite a idade da terceira pessoa: ");
-    scanf("%d", &idade3);
+    scanf("%d", &idade3_563);
 
-    maior = maiorIdade(idade1, idade2, idade3);
-    menor = menorIdade(idade1, idade2, idade3);
+    maior563 = maiorIdade563(idade1_563, idade2_563, idade3_563);
+    menor563 = menorIdade563(idade1_563, idade2_563, idade3_563);
 
-    printf("A maior idade é: %d\n", maior);
-    printf("A menor idade é: %d\n", menor);
+    printf("A maior idade é: %d\n", maior563);
+    printf("A menor idade é: %d\n", menor563);
 
     return 0;
 }
 
-int maiorIdade(int a, int b, int c) {
-    int maior;
+int maiorIdade563(int a563, int b563, int c563) {
+    int maior563;
 
-    if (a > b && a > c) {
-        maior = a;
-    } else if (b > a && b > c) {
-        maior = b;
+    if (a563 > b563 && a563 > c563) {
+        maior563 = a563;
+    } else if (b563 > a563 && b563 > c563) {
+        maior563 = b563;
     } else {
-        maior = c;
+        maior563 = c563;
     }
 
-    return maior;
+    return maior563;
 }
 
-int menorIdade(int a, int b, int c) {
-    int menor;
+int menorIdade563(int a563, int b563, int c563) {
+    int menor563;
 
-    if (a < b && a < c) {
-        menor = a;
-    } else if (b < a && b < c) {
-        menor = b;
+    if (a563 < b563 && a563 < c563) {
+        menor563 = a563;
+    } else if (b563 < a563 && b563 < c563) {
+        menor563 = b563;
     } else {
-        menor = c;
+        menor563 = c563;
     }
 
-    return menor;
+    return menor563;
 }
-
 
 
