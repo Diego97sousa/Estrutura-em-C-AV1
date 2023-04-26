05108dcd9f60ba66eb278d641ab31d2f93420780
Diego97sousa <116231789+Diego97sousa@users.noreply.github.com>
2023-04-26 20:26:54 -0300

    Update Questão 3.c

--- Questão 3.c
+++ Questão 3.c
@@ -2,36 +2,36 @@
 
 int main() {
     
-    int grades[10];
-    int i, soma = 0;
-    int maior, menor;
+    int grades_563[10];
+    int i_563, soma_563 = 0;
+    int maior_563, menor_563;
 
-         printf("Questão 4: \n\n ");
-    for (i = 0; i < 10; i++) {
-        printf("Digite a nota do aluno %d: ", i+1);
-        scanf("%d", &grades[i]);
-        soma += grades[i];
+         printf("Questão 3: \n\n ");
+    for (i_563 = 0; i_563 < 10; i_563++) {
+        printf("Digite a nota do aluno %d: ", i_563+1);
+        scanf("%d", &grades_563[i]);
+        soma_563 += grades_563[i_563];
     }
 
 
-    float media = (float) soma / 10;
+    float media_563 = (float) soma_563 / 10;
 
 
-    maior = grades[0];
-    menor = grades[0];
-    for (i = 1; i < 10; i++) {
-        if (grades[i] > maior) {
-            maior = grades[i];
+    maior_563 = grades_563[0];
+    menor_563 = grades_563[0];
+    for (i_563 = 1; i_563 < 10; i_563++) {
+        if (grades_563[i] > maior_563) {
+            maior_563 = grades_563[i];
         }
-        if (grades[i] < menor) {
-            menor = grades[i];
+        if (grades_563[i_563] < menor_563) {
+            menor_563 = grades_563[i_563];
         }
     }
 
-    printf("Soma total das notas: %d\n", soma);
-    printf("Media das notas: %.2f\n", media);
-    printf("Maior nota: %d\n", maior);
-    printf("Menor nota: %d\n", menor);
+    printf("soma total das notas: %d\n", soma_563);
+    printf("média das notas: %.2f\n", media_563);
+    printf("A maior nota: %d\n", maior_563);
+    printf("A menor nota: %d\n", menor_563);
 
     return 0;
 }
