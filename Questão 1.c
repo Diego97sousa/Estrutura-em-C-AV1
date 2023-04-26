ab36422fb6b18682751c9ce51c7627dd65a7d13d
Diego97sousa <116231789+Diego97sousa@users.noreply.github.com>
2023-04-26 20:05:00 -0300

    Update Questão 1.c

--- Questão 1.c
+++ Questão 1.c
@@ -1,24 +1,27 @@
+#include <stdio.h>
+
 int main() {
     
-    float valorCompra, valorDesconto, valorFinal;
+    float valor_compra_7563, valor_desconto_7563, valor_final_7563;
     
-    printf("Informe o valor da compra: R$ ");
-    scanf("%f", &valorCompra);
+    printf("Questão 1");
+    printf("Olá, informe o valor da compra aqui: R$ ");
+    scanf("%f", &valor_compra_7563);
 
-    if(valorCompra <= 5000) {
-        valorDesconto = valorCompra * 0.12;
-    } else if(valorCompra > 5000 && valorCompra <= 8000) {
-        valorDesconto = valorCompra * 0.07;
-    } else if(valorCompra > 8000 && valorCompra <= 10000) {
-        valorDesconto = valorCompra * 0.04;
+    if(valor_compra_7563 <= 5000) {
+        valor_desconto_7563 = valor_compra_7563 * 0.12;
+    } else if(valor_compra_7563 > 5000 && valor_compra_7563 <= 8000) {
+        valor_desconto_7563 = valor_compra_7563 * 0.07;
+    } else if(valor_compra_7563 > 8000 && valor_compra_7563 <= 10000) {
+        valor_desconto_7563 = valor_compra_7563 * 0.04;
     } else {
-        valorDesconto = valorCompra * 0.03;
+        valor_desconto_7563 = valor_compra_7563 * 0.03;
     }
 
-    valorFinal = valorCompra - valorDesconto;
+    valor_final_7563 = valor_compra_7563 - valor_desconto_7563;
 
-    printf("Valor do desconto: R$ %.2f\n", valorDesconto);
-    printf("Valor final a pagar: R$ %.2f\n", valorFinal);
+    printf("Valor do desconto: R$ %.2f\n", valor_desconto_7563);
+    printf("Valor final a pagar: R$ %.2f\n", valor_final_7563);
 
     return 0;
 }
