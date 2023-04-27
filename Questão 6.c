cdc9f444cc312885f18374e0c480100a91ba5911
Diego97sousa <116231789+Diego97sousa@users.noreply.github.com>
2023-04-26 21:04:38 -0300

    Update Questão 6.c

--- Questão 6.c
+++ Questão 6.c
@@ -1,35 +1,37 @@
 #include <stdio.h>
 
-void menor_maior(int notas[], int tamanho, int *menor, int *maior) {
-    int i;
+void menor_maior563(int notas563[], int tamanho563, int *menor563, int *maior563) {
+    int i_563;
     
-    *menor = notas[0];
-    *maior = notas[0];
+    *menor563 = notas563[0];
+    *maior563 = notas563[0];
     
-    for (i = 1; i < tamanho; i++) {
-        if (notas[i] < *menor) {
-            *menor = notas[i];
+    for (i_563 = 1; i_563 < tamanho563; i_563++) {
+        if (notas563[i_563] < *menor563) {
+            *menor563 = notas563[i_563];
         }
-        if (notas[i] > *maior) {
-            *maior = notas[i];
+        if (notas563[i_563] > *maior563) {
+            *maior563 = notas563[i_563];
         }
     }
 }
 
 int main() {
-    int notas[4];
-    int i, menor, maior;
+    int notas563[4];
+    int i_563, menor563, maior563;
     
+    printf("Questão 6\n\n");
     printf("Digite as notas dos quatro alunos:\n");
-    for (i = 0; i < 4; i++) {
-        scanf("%d", &notas[i]);
+    for (i_563 = 0; i_563 < 4; i_563++) {
+        scanf("%d", &notas563[i_563]);
     }
     
-    menor_maior(notas, 4, &menor, &maior);
+    menor_maior563(notas563, 4, &menor563, &maior563);
     
-    printf("Menor nota: %d\n", menor);
-    printf("Maior nota: %d\n", maior);
+    printf("Menor nota: %d\n", menor563);
+    printf("Maior nota: %d\n", maior563);
     
     return 0;
 }
+
 
